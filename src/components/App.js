3b6a1fe1dc90ce9a259ce666faa4392a19836bc6
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-13 11:23:49 +0200

    MOdify App component to become a class

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,24 @@ import Display from './Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
