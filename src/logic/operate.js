a17bcfa0a08ec6bd1470af50921ba4ef6c2435b8
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-13 10:12:05 +0200

    create operate method

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,7 +1,27 @@
 import Big from './big.mjs';
 
 const operate = (numberOne, numberTwo, operation) => {
-
+  let result;
+  switch (operation) {
+    case '+':
+      result = Big(numberOne).plus(Big(numberTwo));
+      break;
+    case '-':
+      result = Big(numberOne).minus(Big(numberTwo));
+      break;
+    case 'X':
+      result = Big(numberOne).times(Big(numberTwo));
+      break;
+    case '÷':
+      result = Big(numberOne).div(Big(numberTwo));
+      break;
+    case '%':
+      result = Big(numberOne).mod(Big(numberTwo));
+      break;
+    default:
+      result;
+  }
+  return result;
 };
 
 export default operate;
